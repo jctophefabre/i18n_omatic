27759da9cff43b20620092965905024e21d1679a
Jean-Christophe Fabre <jctophe.fabre@gmail.com>
2021-01-25 21:12:11 +0100

    Display help for the update command-line tool
    * Added --help flag to display help
    * Fixed potential bug when providing
      a non-existing command line argument
    
      (closes #5)

--- bin/update.dart
+++ bin/update.dart
@@ -5,11 +5,39 @@ import 'package:i18n_omatic/i18n_omatic_generator.dart';
 void main(List<String> args) {
   var parser = ArgParser();
   parser
-    ..addOption('src-dir', defaultsTo: 'lib')
-    ..addOption('out-dir', defaultsTo: 'assets/i18nomatic');
-  var parsedArgs = parser.parse(args);
+    ..addOption(
+      'src-dir',
+      defaultsTo: 'lib',
+      help: 'path to sources directory',
+    )
+    ..addOption(
+      'out-dir',
+      defaultsTo: 'assets/i18nomatic',
+      help: 'path to translations files directory',
+    )
+    ..addFlag(
+      'help',
+      negatable: false,
+      help: 'displays this help message',
+    );
 
-  var gen = I18nOMaticGenerator(parsedArgs['src-dir'], parsedArgs['out-dir']);
+  var parsedArgs;
+  try {
+    parsedArgs = parser.parse(args);
+  } on ArgParserException catch (e) {
+    print(e.message);
+    print('');
+    print('Usage:');
+    print(parser.usage);
+  }
 
-  gen.run();
+  if (parsedArgs != null) {
+    if (parsedArgs['help']) {
+      print(parser.usage);
+    } else {
+      var gen =
+          I18nOMaticGenerator(parsedArgs['src-dir'], parsedArgs['out-dir']);
+      gen.run();
+    }
+  }
 }
